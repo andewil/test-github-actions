86d28f392dea6b06f2e1ca011eb64411411a8d23
andewil <voronin@andewil.com>
2024-11-03 19:31:30 +0100

    Initial commit

---
+++ extract-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+branch_name="${GITHUB_REF#refs/heads/}"
+
+if [[ "$branch_name" =~ release/([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+  version="${BASH_REMATCH[1]}"
+  echo "Extracted Version: $version"
+  echo "RELEASE_VERSION=${version}" >> ${GITHUB_ENV}
+else
+  echo "RELEASE_VERSION=${branch_name}" >> ${GITHUB_ENV}
+fi
